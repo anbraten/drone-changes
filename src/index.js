5a7bd1270f0683886cc9b53befcf2719e674d1a3
Anton Bracke <anton@ju60.de>
2020-08-07 01:20:21 +0200

    fix env loading

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const { getGitDiffFiles } = require('./git-diff');
-const { ingoreFiles, matchesFiles, ingoreFiles, hasMatches } = require('./glob');
+const { ingoreFiles, matchesFiles, hasMatches } = require('./glob');
 
 async function init() {
   const before = process.env.DRONE_COMMIT_BEFORE;
   const after = process.env.DRONE_COMMIT_AFTER;
-  const includes = process.env.PLUGIN_INCLUDES;
-  const excludes = process.env.PLUGIN_EXCLUDES;
+
+  let includes = process.env.PLUGIN_INCLUDES;
+  if (typeof includes === 'string') {
+    includes = includes.split(',').map(i => i.trim());
+  }
+
+  let excludes = process.env.PLUGIN_EXCLUDES;
+  if (typeof excludes === 'string') {
+    excludes = excludes.split(',').map(i => i.trim());
+  }
 
   console.log('includes', includes);
   console.log('excludes', excludes);
