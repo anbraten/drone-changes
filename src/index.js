f08b6faf27c722c5525903e1b309d1e0703185b1
Lukas Hass <lukas@slucky.de>
2020-09-03 00:23:44 +0200

    refactor: explicitly detect event type

--- src/index.js
+++ src/index.js
@@ -2,26 +2,33 @@ const { getGitDiffFiles } = require('./git-diff');
 const { ingoreFiles, matchesFiles, hasMatches } = require('./glob');
 
 async function init() {
-  let before = process.env.DRONE_COMMIT_BEFORE;
-  const after = process.env.DRONE_COMMIT_AFTER;
+  const event = process.env.DRONE_BUILD_EVENT;
+
+  let before;
+  let after;
+
+  switch (event) {
+    case 'push':
+      before = process.env.DRONE_COMMIT_BEFORE;
+      after = process.env.DRONE_COMMIT_AFTER;
+    
+      if (/^0+$/.test(before)) {
+        console.log('new branch => continue ci execution');
+        process.exit(0);
+      }
+      break;
+    case 'pull_request':
+      before = process.env.DRONE_TARGET_BRANCH;
+      after = process.env.DRONE_COMMIT;
+      break;
+    default:
+      console.log(`event ${event} not supported => continue ci execution`);
+      process.exit(0);
+  }
+
+  console.log('event', event);
   console.log('before', before);
   console.log('after', after);
-  
-  const pullRequest = process.env.DRONE_PULL_REQUEST;
-  if (pullRequest) {
-    const targetBranch = process.env.DRONE_TARGET_BRANCH;
-    console.log('pull request', pullRequest);
-    console.log('target branch', targetBranch);
-
-    console.log('pull request => set before to target branch');
-    before = targetBranch;
-  }
-
-  if (/^0+$/.test(before)) {
-    console.log('new branch => continue ci execution');
-    process.exit(0);
-    return;
-  }
 
   let includes = process.env.PLUGIN_INCLUDES || '';
   let excludes = process.env.PLUGIN_EXCLUDES || '';
